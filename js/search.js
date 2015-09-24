5cfef075c75ec1a968758fa758bf8996b78c0772
Matt Altus <matt@altus.id.au>
2015-09-24 15:35:24 +0930

    Add infinite scroll to results.

--- js/search.js
+++ js/search.js
@@ -1,3 +1,5 @@
+var last_search;
+
 function result_table_init () {
     $('#results_table').html('');
     $('#results_table').children('table').remove();
@@ -13,12 +15,12 @@ function result_table_init () {
             $('<th>').text('Download'));
 }
 
-function result_table_append (list) {
+function result_table_append (list, offset) {
     var results_table = $('#results_table').children();
 
     $.each(list, function (index, element) {
         var links = '<a href="' + element.link + '"><span class="glyphicon glyphicon-download"></span></a>';
-        links += sab_link_get_html(index);
+        links += sab_link_get_html(index + offset);
 
         $('<tr>').append(
                 $('<td>').text(jQuery.timeago(Date.parse(element.pubDate))),
@@ -28,15 +30,20 @@ function result_table_append (list) {
                 $('<td>').html(links)
                 ).appendTo(results_table);
 
-        sab_link_init(index, element.link, element.title);
+        sab_link_init(index + offset, element.link, element.title);
     });
 }
 
 function result_table_populate(search_type, search_string, search_cat, search_offset) {
+    // Stop another infinite scroll load happening at the same time.
+    last_search = null;
+
+    $('#results_spinner').html('<div class="text-center"><span class="glyphicon glyphicon-refresh glyphicon-spin"></span></div>');
+
     var params = {
         t      : search_type,
         o      : 'json',
-        limit  : 100,
+        limit  : 20,
         offset : search_offset,
         apikey : $.cookie('apikey'),
         q      : search_string,
@@ -54,6 +61,8 @@ function result_table_populate(search_type, search_string, search_cat, search_of
     var url = pynab_url() + $.param(params);
 
     $.getJSON(url, function (data) {
+        $('#results_spinner').html('');
+
         // Check for an error from pynab.
         if (data.error) {
             if (search_offset == 0) {
@@ -63,12 +72,20 @@ function result_table_populate(search_type, search_string, search_cat, search_of
             return;
         }
 
+        // If it's a new table populate the table headers.
         if (search_offset == 0) {
             result_table_init();
         }
-        result_table_append(data.rss.channel.item);
 
+        // If we have data, add it to the table and set last_search so infinite
+        // scroll works again.
+        if (data.rss.channel.item) {
+            result_table_append(data.rss.channel.item, search_offset);
+            last_search = params;
+         }
     }).fail(function (data, status, msg) {
+        $('#results_spinner').html('');
+
         if (search_offset == 0) {
             $('#results_table').html('');
         }
@@ -77,8 +94,14 @@ function result_table_populate(search_type, search_string, search_cat, search_of
 }
 
 function do_search (search_type, search_string, search_cat) {
-    $('#results_table').html('<div class="text-center"><span class="glyphicon glyphicon-refresh glyphicon-spin"></span></div>');
     result_table_populate(search_type, search_string, search_cat, 0);
+}
+
+function do_scroll () {
+    if (last_search) {
+        last_search['offset'] += last_search['limit'];
+        result_table_populate(last_search['t'], last_search['q'], last_search['cat'], last_search['offset']);
+    }
 }
 
 $(document).ready(function() {
@@ -96,5 +119,31 @@ $(document).ready(function() {
     });
 
     $('#search_string').focus();
+
+    // Infinite script for when the document is smaller than the window.
+    $(document).keydown(function(e) {
+        if ($(document).height() <= $(window).height()) {
+            if (e.which == 34 || // PgDown
+                e.which == 40 || // Down
+                e.which == 35) { // End
+                do_scroll();
+            }
+        }
+    });
+
+    // Infinite script for when the document is smaller than the window.
+    $(window).bind('mousewheel DOMMouseScroll', function(event){
+        if ($(document).height() <= $(window).height()) {
+            do_scroll();
+        }
+    });
+
+    // Infinite scroll.when the document is bigger than the window.
+    $(window).scroll(function(){
+        var scrolltrigger = 0.95;
+        if (($(window).scrollTop() / ($(document).height() - $(window).height())) > scrolltrigger) {
+            do_scroll();
+        }
+    });
 });
 
